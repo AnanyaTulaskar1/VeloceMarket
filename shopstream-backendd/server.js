5a266259f43cc86b04585824aaa023b22f489817
AnanyaTulaskar1 <ananyatulaskar49@gmail.com>
2026-06-03 03:22:59 +0530

    Fix full-stack CORS policy configurations and port handling

--- shopstream-backendd/server.js
+++ shopstream-backendd/server.js
@@ -12,8 +12,10 @@ const app = express();
 
 // Global Middlewares
 app.use(cors({
-  origin: 'http://localhost:5173', // Allows your Vite React app to talk to the backend
-  credentials: true
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
